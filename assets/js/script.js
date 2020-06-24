ca8fbb266ce45cf8426bdd19f23e00c638fbae52
BG00924 <BG00924@users.noreply.github.com>
2020-06-24 14:09:25 -0500

    added type and js to add it to the list

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,13 +15,26 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function() {
     // prevents automatic refresh of the page
     event.preventDefault();
+    // var for task input field speifically selecting and storing value
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    // variable declaration for drop down menu input
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     // creates the li
     var listItemEl = document.createElement("li");
-    // applies task-item css style
-    listItemEl.className = "task-item";
+    listItemEl.className = "task-item"; //applies the task-item class to each created li
+    //creat div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // gives the div a class name
+    taskInfoEl.className = "task-info";
+    // adds html content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
     // add text inside the lo
-    listItemEl.textContent = "This is a new task.";
-    // applies the li to at the end of the element/container
+    // listItemEl.textContent = "This is a new task.";
+    // adjusted from about to pull the var taskNameInput which selected and stored the value input into the field
+    //listItemEl.textContent = taskNameInput;
+    // add type into the list item 
+    listItemEl.appendChild(taskInfoEl);
+    // adds entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 
 };
